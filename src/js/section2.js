60d1d89988366f2d2c3f776cda8d9fb6909b6d3a
introduce131 <introduce131@naver.com>
2023-05-25 12:39:48 +0900

    2023-05-25

--- src/js/section2.js
+++ src/js/section2.js
@@ -4,3 +4,35 @@
     opacity : 0 → 0.25 → 0.75 → 1 → 0.75 → 0.25
     perspctive : 100px 고정
 */
+
+var list = document.querySelectorAll(".keywordList__container ul");
+var listArray = list[0].children;
+
+const listContent = [
+    "이름 : 백종원",
+    "#긍정적 #꼼꼼함 #끈기",
+    "취미 : ㅇㅇㅇ",
+    "MBTI : ISTJ",
+    "E-mail : introduce131@gmail.com",
+    "생년월일 : 01.01.27"
+];
+
+console.log(listArray);
+
+for(let i=0; i<listArray.length; i++) {
+    console.log(listArray[i].getBoundingClientRect().x);
+    console.log(listArray[i].getBoundingClientRect().y);
+}
+
+
+i = 0;  // 돌려쓰기 위해서 0으로 초기화함
+
+setInterval(()=> {
+    if(i >= listArray.length) i=0;
+
+    for(i=0; i<listArray.length; i++) {
+        console.log(i);
+        // let myOwnValue = listArray[i].getBoundingClientRect().y;
+        // listArray[i].setAttribute('style', `top: ${(myOwnValue + 68)}px`);
+    }
+}, 2000)
