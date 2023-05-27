7d163e6cede1633db9d44b043329634fb1d5f11b
jongwon Baek <introduce131@naver.com>
2023-05-28 02:23:52 +0900

    2023

--- src/js/section2.js
+++ src/js/section2.js
@@ -4,3 +4,32 @@
     opacity : 0 → 0.25 → 0.75 → 1 → 0.75 → 0.25
     perspctive : 100px 고정
 */
+
+var listArray = document.querySelectorAll(".keywordList__container ul")[0].children;
+
+console.log(listArray);
+
+for (let i = 0; i < listArray.length; i++) {
+  console.log(listArray[i].getBoundingClientRect());
+  listArray[i].setAttribute("style", `top:${(i + 1) * 68}px`);
+}
+
+i = 0; // 돌려쓰기 위해서 0으로 초기화함
+
+setInterval(() => {
+  if (i >= listArray.length) i = 0;
+
+  const sltdItem = listArray[i];
+  sltdItem.setAttribute("style", `top:${0}px;`);
+
+  // 트랜지션 시간(0.5초) 후, 다시 초기화
+  setTimeout(() => {}, 500);
+
+  i++;
+
+  // for (i = 0; i < listArray.length; i++) {
+  //   let myOwnValue = listArray[i].getBoundingClientRect().top;
+  //   listArray[i].setAttribute("style", `top: ${myOwnValue + 68}px`);
+  //   console.log(`${i + 1}번째 li의 top값에 ` + (myOwnValue + 68) + "px 넣었음");
+  // }
+}, 2000);
